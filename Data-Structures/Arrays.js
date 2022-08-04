f66bf8c4d683f98c8503079b5fd6d794b448ba25
Omar Hosny <honsyomar022@gmail.com>
2022-08-04 07:29:17 +0200

    Array Methods and Time Complexity

--- Data-Structures/Arrays.js
+++ Data-Structures/Arrays.js
@@ -5,3 +5,25 @@ Some Info About Arrays
 3- insert => O(n)
 4- delete => O(n)
 */
+
+const Strings = ["a", "b", "c", "d"];
+
+//Accessing
+Strings[0]; // O(1)
+
+// push
+Strings.push("e"); // O(1)
+
+// pop
+Strings.pop(); // O(1)
+
+//unshifting
+Strings.unshift("x"); // O(n)
+
+//insert
+Strings.splice(2, 0, "e"); // O(n)
+
+//delete
+Strings.splice(2, 1); // O(n)
+
+console.log(Strings);
