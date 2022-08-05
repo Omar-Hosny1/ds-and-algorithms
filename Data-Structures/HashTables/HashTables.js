beeed293117b20d22a513358abec37be1ea6cf46
Omar Hosny <honsyomar022@gmail.com>
2022-08-05 09:08:54 +0200

    Some info about hashtables added

--- Data-Structures/HashTables/HashTables.js
+++ Data-Structures/HashTables/HashTables.js
@@ -5,3 +5,23 @@ Some Info About Hash Tables
 3- insert => O(1)
 4- delete => O(1)
 */
+
+let user = {
+  name: "omar",
+  age: 18,
+  magic: true,
+  sayHello: () => console.log("Hello!"),
+};
+
+//ACCESSING
+user.age; // O(1)
+
+// INSERT
+user.gender = "MALE"; // O(1)
+
+/*
+! REMEMBER THE KEYS IN THE OBJECTS IS ALWAYS STRING
+! YOU CAN STORE KEYS IN DIFFRENT DATATYPES USING MAP
+*/
+
+let MAP = new Map(); // MAP
