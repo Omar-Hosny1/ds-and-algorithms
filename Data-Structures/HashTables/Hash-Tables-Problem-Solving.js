b4f66f4f2c9d96acec0d6c14288f1163e781704f
Omar Hosny <honsyomar022@gmail.com>
2022-08-05 10:49:19 +0200

    one more problem solved

--- Data-Structures/HashTables/Hash-Tables-Problem-Solving.js
+++ Data-Structures/HashTables/Hash-Tables-Problem-Solving.js
@@ -2,11 +2,30 @@ const firstRepeatedCharacter = (array) => {
   let MAP = new Map();
   for (let i = 0; i < array.length; i++) {
     if (MAP.get(array[i]) == undefined) {
-      MAP.set(array[i], false);
-    } else if (MAP.get(array[i]) == false) {
-      return array[i];
+      MAP.set(array[i], 1);
+    } else if (MAP.get(array[i])) {
+      const numOfRepeating = MAP.get(array[i]);
+      MAP.set(array[i], numOfRepeating + 1);
+    }
+  }
+  const GreatestRe = Math.max(...MAP.values());
+
+  for (let item of MAP) {
+    if (item[1] == GreatestRe) return item[0];
+  }
+
+  return GreatestRe;
+};
+console.log(firstRepeatedCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+
+const firstRepeatedCharacterTwo = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] == array[j]) {
+        return array[i];
+      }
     }
   }
   return "No Repeated Characters!";
 };
-// console.log(firstRepeatedCharacter([2, 3, 1, 2]));
+console.log(firstRepeatedCharacterTwo([3, 1, 2, 1, 3]));
