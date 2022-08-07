8fadcddbf6d7553d1abaa54c2e0bd836ed36742e
Omar Hosny <honsyomar022@gmail.com>
2022-08-07 23:53:46 +0200

    Some Information About LinkedLists Added

---
+++ Data-Structures/LinkedLists/LinkedList.js
@@ -0,0 +1,14 @@
+/*
+Some Informations About Linked Lists : 
+1 - LinkedList Has 2 Elements The First One Is The Value And The Second One Is The Pointer To The Next Linked List  (NODE)
+2 - The First Node Called Head
+3 - The Last Node Called Tail
+4 - The Last Node Point To NULL
+
+Linked List Time Complexity : 
+1- Prepend => O(1)
+2- Append => O(1)
+3- Insert => O(n)
+4- Delete => O(n)
+5- Lookup => O(n)
+*/
