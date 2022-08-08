8306f3a5c55b929ccef0f732237496d606d3c7f5
Omar Hosny <honsyomar022@gmail.com>
2022-08-08 03:22:12 +0200

    All Method Added

--- Data-Structures/LinkedLists/LinkedList-Implementaion.js
+++ Data-Structures/LinkedLists/LinkedList-Implementaion.js
@@ -24,107 +24,80 @@ class Node {
     }
 }
 */
-
 class LinkedList {
   constructor(value) {
-    this.head = { value, next: null };
+    this.head = {
+      value: value,
+      next: null,
+    };
     this.tail = this.head;
     this.length = 1;
   }
-
-  printList() {
-    let LinkedList = [];
-    let currentNode = this.head;
-    while (currentNode !== null) {
-      LinkedList.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    return LinkedList;
-  }
-
-  traverseToIndex(index) {
-    let conter = 0;
-    let replacedNode = this.head;
-    while (index !== conter) {
-      replacedNode = replacedNode.next;
-      conter++;
-    }
-    return replacedNode;
-  }
-  insert(index, value) {
-    if (index > this.length) {
-      return this.append(value);
-    }
-    if (index == 0) {
-      return this.prepend(value);
-    }
-    const newNode = { value, next: null };
-    // let conter = 0;
-    // let replacedNode = this.head;
-    // while (index !== conter) {
-    //   replacedNode = replacedNode.next;
-    //   conter++;
-    // } *THIS LOGIC ADDED TO A UNIQE METHOD TO KEEP THE CODE CLEAN* => traverseToIndex
-    let replacedNode = this.traverseToIndex(index);
-    newNode.next = replacedNode;
-    this.linkThePointers(index, newNode);
-    // let count = 0;
-    // let mainLinked = this.head;
-    // while (count <= index) {
-    //   count++;
-    //   if (count == index) {
-    //     mainLinked.next = newNode;
-    //   }
-    //   mainLinked = mainLinked.next;
-    // } *THIS LOGIC ADDED TO A UNIQE METHOD TO KEEP THE CODE CLEAN* => linkThePointers
-    this.length++;
-    return this;
-  }
-
   append(value) {
     const newNode = {
       value: value,
       next: null,
     };
-    let count = 0;
-    let head = this.head;
-    while (count <= this.length) {
-      count++;
-      if (count == this.length) {
-        head.next = newNode;
-      }
-      head = head.next;
-    }
+    this.tail.next = newNode;
+    this.tail = newNode;
     this.length++;
     return this;
   }
-
   prepend(value) {
-    let newNode = { value: value, next: null };
+    const newNode = {
+      value: value,
+      next: null,
+    };
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
   }
-
-  linkThePointers(index, newNode) {
-    let count = 0;
-    let mainLinked = this.head;
-    while (count <= index) {
-      count++;
-      if (count == index) {
-        mainLinked.next = newNode;
-      }
-      mainLinked = mainLinked.next;
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
     }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      console.log("yes");
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
   }
 }
 
 const myLinkedList = new LinkedList(1);
-
-myLinkedList.append(2);
-myLinkedList.append(4);
-myLinkedList.prepend(0);
-myLinkedList.insert(2, 3);
-
-console.log(myLinkedList.printList());
