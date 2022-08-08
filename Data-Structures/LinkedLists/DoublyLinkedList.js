fdbccbc69f1db1825923120ba4a73e0a15c57a6c
Omar Hosny <honsyomar022@gmail.com>
2022-08-08 06:00:43 +0200

    Doubly Linked List Added

---
+++ Data-Structures/LinkedLists/DoublyLinkedList.js
@@ -0,0 +1,4 @@
+/*
+The Main Deffrence Between The Single Linked List And The Double Linked List Is That
+Double Linked List Has A 2 Pointers One For The Prev Element And One For The Next Element
+*/
