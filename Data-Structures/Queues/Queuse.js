e6ec52a52016183e7cc1d6a5a72effa7e1f01be9
Omar Hosny <honsyomar022@gmail.com>
2022-08-09 05:09:13 +0200

    Some Info Added About Queues

---
+++ Data-Structures/Queues/Queuse.js
@@ -0,0 +1,9 @@
+/**
+ * Lookup O(n)
+ * Enqueue O(1)   // PUSH
+ * Dequeue O(1)  // POP
+ * Peek O(1)
+ *
+ * QUEUES => FIFO (First In First Out).
+ * The First Item That Comes In Is The Fitst One That Comes Out.
+ * */
--- Data-Structures/Stacks/Stack.js
+++ Data-Structures/Stacks/Stack.js
@@ -5,5 +5,5 @@
  * Peek O(1)
  *
  * STACK => LIFO (Last In First Out).
- * The Last Item That Comes In Is The Fitst One That Comes Out
+ * The Last Item That Comes In Is The Fitst One That Comes Out.
  * */
