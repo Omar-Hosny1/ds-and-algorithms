845842c8771693f2c862a57d4fa7c3e2a8068da1
Omar Hosny <honsyomar022@gmail.com>
2022-08-12 08:11:01 +0200

    Binary Tree Added

---
+++ Data-Structures/Trees/BinarySearchTree.js
@@ -0,0 +1,11 @@
+/*
+     101
+   /     \ 
+  33      105
+ /  \    /    \ 
+9   37  104   144
+
+* By Keep Going To The Right The Value Will Increases 
+* By Keep Going To The Left The Value Will Decreasing 
+* Visiual ALgo => https://visualgo.net/en/bst 
+ */
