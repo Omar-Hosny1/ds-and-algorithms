c3a5faf708b77ce00b4e46a9c6f047c8666b6c8d
Omar Hosny <honsyomar022@gmail.com>
2022-08-12 08:45:20 +0200

    Some Info Adde About Binary Tree

--- Data-Structures/Trees/BinaryTree.js
+++ Data-Structures/Trees/BinaryTree.js
@@ -1,8 +1,16 @@
 /**
+ * Balanced Binary Tree {
  * Lookup O(Log N)
  * Insert O(Log N)
  * Delete O(Log N)
  * Num Of Nodes = (2 ^ H) - 1;
  * H = ( The Height Of The Tree )
  * log 100 = 2 because 10 ^ 2 = 100
- * */
+ * }
+ *
+ * Unbalanced Binary Tree{
+ * Lookup O(N)
+ * Insert O(N)
+ * Delete O(N)
+ * }
+ */
