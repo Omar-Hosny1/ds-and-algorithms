fefeeeaaebe75e4073d83dd8ff6c47faa7e79962
Omar Hosny <honsyomar022@gmail.com>
2022-08-13 23:31:02 +0200

    Algorthims Section Added

---
+++ Algorthims/Recursion.js
@@ -0,0 +1,42 @@
+/**
+ * To Build A Recursive Function :
+ * 1- Identify The Base Case
+ * 2- Identify The Recursive Case
+ * 3- Return When Needed. (USUALLY YOU HAVE TWO RETURNS)/
+ */
+
+let counter = 0;
+function inception() {
+  if (counter > 3) {
+    return "done";
+  }
+  counter++;
+  return inception();
+}
+
+// console.log(inception());
+
+// THE MOST PROPLEMS THAT YOU CAN USE RECURSIVE TO SOLVE IT.
+
+function factorialNumber(num) {
+  if (num == 2) {
+    return 2;
+  }
+  return num * factorialNumber(num - 1);
+}
+
+// console.log(factorialNumber(5));
+
+function fibonacciNumber(n) {
+  if (n < 2) return n;
+  return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
+}
+
+// console.log(fibonacciNumber(3));
+
+function reversrString(s) {
+  if (s.length == 0 || s.length == 1) {
+    return s;
+  }
+  return reversrString(s[0]);
+}
--- Data-Structures/Arrays/ArraysProblemSolving.js
+++ Data-Structures/Arrays/ArraysProblemSolving.js
@@ -298,4 +298,4 @@ var findDisappearedNumbers = function (nums) {
   }
   return result;
 };
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+// console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
